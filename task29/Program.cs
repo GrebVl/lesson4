c92258868987cdd15a1e2e6b018ca3406fb5f738
Vladimir <chertonovskiy@icloud.com>
2022-08-08 00:18:17 +0700

    first commit task

---
+++ task29/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+программа задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] fillingArray(int number)
+{
+    int[] Arr = new int[number];
+    Random rnd = new Random();
+
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Arr[i] = rnd.Next(0, 100);
+    }
+    return Arr;
+}
+
+Console.WriteLine("Введите длину массива:");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = fillingArray(size);
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (j < array.Length - 1)
+    {
+        Console.Write(array[j] + ", ");
+    }
+    else
+    {
+        Console.Write(array[j] + " ");
+    }
+}
